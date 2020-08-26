5303912f95ad3bee24f0b05064dcb73e7abeadc3
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-08-26 13:08:12 -0400

    [add] Cleaned up DL flask api

--- docker-and-aws/lesson/flask/app.py
+++ docker-and-aws/lesson/flask/app.py
@@ -1,8 +1,7 @@
-# Make a flask API for our DL Model
-
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 
+# Import libraries
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 from flask_restplus import Api, Resource, fields
@@ -10,17 +9,19 @@ from flask import Flask, request, jsonify
 import numpy as np
 from werkzeug.datastructures import FileStorage
 from PIL import Image
-from keras.models import model_from_json
 import tensorflow as tf
 
 
+# Define app and API
 app = Flask(__name__)
 api = Api(app, version='1.0', title='MNIST Classification', description='CNN for Mnist')
-ns = api.namespace('Make_School', description='Methods')
+ns = api.namespace('cnn', description='Methods')
 
+# Define parser
 single_parser = api.parser()
 single_parser.add_argument('file', location='files', type=FileStorage, required=True)
 
+# Load model and define tensorflow graph
 model = load_model('my_model.h5')
 graph = tf.get_default_graph()
 
@@ -29,26 +30,34 @@ class CNNPrediction(Resource):
     """Uploads your data to the CNN"""
     @api.doc(parser=single_parser, description='Upload an mnist image')
     def post(self):
+        # Parse args
         args = single_parser.parse_args()
         image_file = args.file
+
+        # Save file and open it again
         image_file.save('mnist.png')
         img = Image.open('mnist.png')
+
+        # Resize and convert to array
         image_red = img.resize((28, 28))
         image = img_to_array(image_red)
-        print(image.shape)
+        print(f"Image shape: {image.shape}")
+
+        # Reshape and scale the image array
         x = image.reshape(1, 28, 28, 1)
         x = x/255
-        # This is not good, because this code implies that the model will be
-        # loaded each and every time a new request comes in.
-        # model = load_model('my_model.h5')
+
+        # Make the prediction
         with graph.as_default():
             out = model.predict(x)
-        print(out[0])
-        print(np.argmax(out[0]))
+
+        # Round
+        print(f"Prediction: {out[0]}")
+        print(f"Argmax of prediction: {np.argmax(out[0])}")
         r = np.argmax(out[0])
 
+        # Return prediction as json
         return {'prediction': str(r)}
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
